0ffcf2bd507944d5ac95bde1ac22922d9325dd4f
ahmadraza330 <ra9557408@gmail.com>
2021-02-22 12:04:58 +0500

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/Homepage';
+import {Route, Switch} from 'react-router-dom';
+
+const Htas = () => {
+  return(
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div>
-     <Homepage/>
+      <div>
+        <Switch>
+        <Route path="/" component={ Homepage} exact={true}/>
+        <Route path="/shop/hats" component={Htas}/>
+        </Switch>
     </div>
+ 
   );
 }
 
